3b762acd3b3b5b3cf549e4c419ed5d3e8378af56
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-14 12:46:02 -0700

    set up offline storage for products

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
-import { useQuery } from '@apollo/react-hooks';
+import React, { useEffect } from "react"
+import { useQuery } from '@apollo/react-hooks'
 import { UPDATE_PRODUCTS } from '../../utils/actions'
 import { useStoreContext } from '../../utils/GlobalState'
-
-import ProductItem from "../ProductItem";
-import { QUERY_PRODUCTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers"
+import ProductItem from "../ProductItem"
+import { QUERY_PRODUCTS } from "../../utils/queries"
 import spinner from "../../assets/spinner.gif"
 
 function ProductList() {
@@ -17,8 +17,15 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products
       })
+      data.products.forEach( product => {
+        idbPromise('products', 'put', product)
+      })
+    } else if (!loading) {
+        idbPromise('products', 'get').then(products => {
+          dispatch({type: UPDATE_PRODUCTS, products})
+        })
     }
-  }, [data, dispatch])
+  }, [data, dispatch, loading])
 
   function filterProducts() {
     if (!currentCategory) {
