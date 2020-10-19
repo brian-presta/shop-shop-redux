394a1112dfd08971530a25459455e20c0d0a1afa
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-19 12:00:46 -0700

    store change actually saved

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import thunkMiddleware from '@reduxjs/toolkit'
 import rootReducer from './reducers'
 import App from '../App'
 
