9d849ea5a7112a136c112fb124bb7daa914b9d0f
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-19 14:35:03 -0700

    bugfixed globalstate.js

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Provider from 'react-redux'
+import { Provider }from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducers'
 
 
 const store = createStore(reducer) 
-export default  StoreProvider = ({...props}) => <Provider store={store} {...props} />
+const StoreProvider = ({...props}) => <Provider store={store} {...props} />
+export default  StoreProvider 
 
 
