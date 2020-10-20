ec144f5489bcbc87033f906ba0e2b4e3f7feac5d
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-19 17:26:50 -0700

    set up heroku seed

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Provider }from 'react-redux'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducers'
 
-
 const store = createStore(reducer) 
 const StoreProvider = ({...props}) => <Provider store={store} {...props} />
+
 export default  StoreProvider 
 
 
